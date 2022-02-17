4144b3f74280a1d47e8162ca249e71d837c412c9
alexey <alexey@digger.dev>
2022-02-17 15:32:10 +0000

    fix slack_webhook_url_ssm value

--- main/main.template.tf
+++ main/main.template.tf
@@ -113,7 +113,7 @@ module "lambda" {
   use_existing_cloudwatch_log_group = true
 
   environment_variables = {
-    ssm_webhook_url = aws_ssm_parameter.slack_webhook_url_ssm.value
+    ssm_webhook_url = aws_ssm_parameter.slack_webhook_url_ssm.arn
   }
 
   allowed_triggers = {
